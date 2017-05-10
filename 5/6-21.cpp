677cc8408a95b678bc051a169bcead53f78a79c5
407962856@qq.com <407962856@qq.com>
2017-05-10 20:02:18 +0800

    作业5

---
+++ 5/6-21.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int countAlphabet(string input) {
+	int count = 0;
+	
+	for (int i = 0; i < input.length(); i++) {
+		if ((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z')) {
+			count++;
+			//cout << "intpu[i] is:" << input[i] << endl;
+		}
+	}
+
+	return count;
+}
+
+int main()
+{
+	string input;
+
+	cout << "Please input your words:" << endl;
+
+	getline(cin,input);
+
+	if (input.empty()) {
+		cout << "-->Please input something!" << endl;
+		cout << "Please press any key to escape." << endl;
+		cin >> input;
+		return -1;
+	}
+	
+	cout << "-->input :" << input << endl;
+	cout << "-->Alphabet number is :" << countAlphabet(input) << endl;
+
+	cout << "Please press any key to escape." << endl;
+	cin >> input;
+
+    return 0;
+}
